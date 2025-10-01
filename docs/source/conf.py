ec1d5d19b4663e35e4796dc40867b498b5e594c1
FFMT <134106339+ffmtsw@users.noreply.github.com>
2025-10-01 22:56:05 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,3 +27,9 @@ html_theme_options = {
     "collapse_navigation": False,
     "navigation_depth": 4,
 }
+
+```python
+html_theme_options = {
+    "collapse_navigation": False,
+    "navigation_depth": 4,
+}
